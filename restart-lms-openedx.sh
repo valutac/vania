dca2d0e7d9b524b451f00239d59d2d4e277cbbad
Rizky Ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2018-08-28 14:07:39 +0700

    restart lms,cms, edxapp_worker and status of open edx

---
+++ restart-lms-openedx.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo /edx/bin/supervisorctl restart lms
--- vania.sh
+++ vania.sh
@@ -4,14 +4,22 @@
 options=(
     "Install Open edX"
     "Generate Certificate"
+    "Check Status"
+    "Restart LMS"
+    "Restart CMS"
+    "Restart edxapp_worker"
     "Quit" 
 )
-PS3="Select your Open edX task (1-3): " 
+PS3="Select your Open edX task (1-7): " 
 
 select option in "${options[@]}"; do
     case "$REPLY" in 
         1) bash install-openedx.sh ;;
         2) bash gencert-openedx.sh ;;
-        3) break ;;
+        3) bash status-openedx.sh ;;
+        4) bash restart-lms-openedx.sh ;;
+        5) bash restart-cms-openedx.sh ;;
+        6) bash restart-edxappworker-openedx.sh ;;
+        7) break ;;
     esac
 done
