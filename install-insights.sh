513de21dcf87923e39c729da9b72694735a1852f
Rizky Ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2018-09-05 22:20:14 +0700

    add release open-release/ginkgo.master, ssh key and trackinglogs

--- install-insights.sh
+++ install-insights.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export OPENEDX_RELEASE=open-release/ginkgo.master
 
 INSIGHTS_URL=""
 LMS_URL=""
@@ -9,13 +10,19 @@ sudo apt-get update -y
 sudo apt-get install -y git python-pip python-dev libmysqlclient-dev python-mysqldb build-essential
 sudo pip install virtualenv
 
+# Generate SSH KEY
+echo "Generate ssh key"
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ''
+echo >> ~/.ssh/authorized_keys # Make sure there's a newline at the end
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
 # Crate Ansible virtualenv
 echo "Create Virtualenv and Activate it"
 virtualenv ansible
 . ansible/bin/activate
 
 # Clone Repository
-git clone https://github.com/ariestiyansyah/configuration.git
+git clone -b $OPENEDX_RELEASE https://github.com/ariestiyansyah/configuration.git
 cd configuration/
 pip install -r requirements.txt
 cd playbooks/edx-east/
@@ -24,6 +31,17 @@ cd playbooks/edx-east/
 echo "Running Ansible, this may take a while..."
 
 ansible-playbook -i localhost, -c local analytics_single.yml --extra-vars "INSIGHTS_LMS_BASE=$LMS_URL INSIGHTS_BASE_URL=$INSIGHTS_URL"
+
+# Copy Trackinglog, make sure you download it before
+echo "copy tracking log"
+cd $HOME
+sudo mkdir -p /edx/var/log/tracking
+sudo cp ~/tracking.log /edx/var/log/tracking
+sudo chown hadoop /edx/var/log/tracking/tracking.log
+
+# Logs to HDFS
+echo "loading logs to HDFS..."
+sleep 60
 
 # Setup Analytic Pipeline
 echo "Setup Analytics Pipeline"
@@ -35,6 +53,6 @@ virtualenv pipeline
 
 # Clone Analytics Pipeline
 echo "Check out pipeline"
-git clone https://github.com/edx/edx-analytics-pipeline
+git clone -b $OPENEDX_RELEASE https://github.com/edx/edx-analytics-pipeline
 cd edx-analytics-pipeline
 
